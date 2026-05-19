003e0dce2510101e2ad84e25712772985d2e0dc5
mmr1337 <kolosovivan39@gmail.com>
2026-05-19 08:32:54 +0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -28,7 +28,7 @@ local Toggles = {}
 local Options = {}
 local Tooltips = {}
 
-local BaseURL = "https://raw.githubusercontent.com/uhfork/Obsidian/refs/heads/main/"
+local BaseURL = "https://raw.githubusercontent.com/mmr1337/Obsidian/refs/heads/main/"
 local CustomImageManager = {}
 local CustomImageManagerAssets = {
     TransparencyTexture = {
